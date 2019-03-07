022d53556b852e3bec9099cd7ca76d0d26ea8418
Matheus Ronfim <mronfim@gmail.com>
2019-03-06 19:54:41 -0500

    Add foundation for Events and Event System

--- Omega/src/Omega/Core.h
+++ Omega/src/Omega/Core.h
@@ -9,3 +9,20 @@
 #else
 	#error Omega only supports Windows!
 #endif
+
+
+#ifdef OMG_DEBUG
+	#define OMG_ENABLE_ASSERTS
+#endif
+
+
+#ifdef OMG_ENABLE_ASSERTS
+	#define OMG_ASSERT(x, ...) { if(!(x)) { OMG_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define OMG_CORE_ASSERT(x, ...) { if(!(x)) { OMG_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define OMG_ASSERT(x, ...)
+	#define OMG_CORE_ASSERT(x, ...)
+#endif
+
+
+#define BIT(x) (1 << x)
